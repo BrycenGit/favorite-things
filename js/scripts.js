cbc295ea3c72ad722514668d26807e7630b83caa
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-08-17 10:15:23 -0700

    add various styles of constants such as brackets, concat, shift and pop to test out
    Co-authored-by: Brycen Bartolome <brycenbartolome@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,22 @@ $(document).ready(function() {
     const inputtedDessert = $("input#dessert").val();
     const inputtedMovie = $("input#movie").val();
     const inputtedFood = $("input#food").val();
-    
+
     const favoriteThings = [inputtedToy, inputtedDessert, inputtedMovie, inputtedFood];
-    
-    $("#output").text(favoriteThings.reverse());
+    const first = favoriteThings[0];
+    const second = favoriteThings[1];
+    const third = favoriteThings[2];
+    const last = favoriteThings[favoriteThings.length - 1];
+    const firstSecond = first.concat(second, third, last);
+    // console.log(last);
+    // const reverseThings = favoriteThings.reverse();
+    // const shift = favoriteThings.shift(2);
+    // let remove = favoriteThings.shift();
+    // console.log(remove);
+    // const itemOne = favoriteThings[3];
+    // const pop = favoriteThings.pop();
+    // console.log(pop);
+    console.log(firstSecond);
+    $("#output").text(firstSecond);
   });
 });
